b8618d8f96f9eff9806cc833d09dd9293bdc2d25
freininghaus <frank78ac@googlemail.com>
2023-09-06 15:12:44 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "pid.h"
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+
+    pid::builder b;
+    pid::builder_offset<std::int32_t> o{b, 12};
+
+    *o = 4;
+}
