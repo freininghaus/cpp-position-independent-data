22cb13e030e2e425514141d87bc82fdeb920a680
Frank Reininghaus <frank78ac@googlemail.com>
2023-09-08 09:56:11 +0200

    Implement strings and vectors

--- pid.h
+++ pid.h
@@ -11,9 +11,11 @@ namespace pid {
     {
         offset_type offset;
 
-        auto & operator=(const T * p)
+        // TODO: make sure that Pointer points to T or const T with std::enable_if
+        template <typename Pointer>
+        auto & operator=(Pointer p)
         {
-            offset = reinterpret_cast<const char *>(p) - reinterpret_cast<const char *>(this);
+            offset = reinterpret_cast<const char *>(&*p) - reinterpret_cast<const char *>(this);
             return *this;
         }
 
@@ -44,12 +46,75 @@ namespace pid {
         }
     };
 
-    template <typename SizeType = std::uint32_t>
-    struct string
+    template <typename SizeType>
+    struct generic_string
     {
-        SizeType size;
+        SizeType string_length;
         char data[];
+
+        SizeType size() const
+        {
+            return string_length;
+        }
+
+        [[nodiscard]] const char * begin() const
+        {
+            return data;
+        }
+
+        [[nodiscard]] const char * end() const
+        {
+            return data + string_length;
+        }
+
+        operator std::string_view() const
+        {
+            return {begin(), end()};
+        }
+
+        template <typename String>
+        bool operator==(const String & other) const
+        {
+            return std::string_view{*this} == other;
+        }
     };
+
+    using string32 = generic_string<std::uint32_t>;
+
+    template <typename T, typename SizeType>
+    struct generic_vector
+    {
+        SizeType vector_length;
+        T data[];
+
+        SizeType size() const
+        {
+            return vector_length;
+        }
+
+        [[nodiscard]] const T * begin() const
+        {
+            return data;
+        }
+
+        [[nodiscard]] const T * end() const
+        {
+            return data + vector_length;
+        }
+
+        T & operator[](SizeType index)
+        {
+            return data[index];
+        }
+
+        const T & operator[](SizeType index) const
+        {
+            return data[index];
+        }
+    };
+
+    template <typename T>
+    using vector32 = generic_vector<T, std::uint32_t>;
 
     template <typename T>
     struct builder_offset;
@@ -68,15 +133,30 @@ namespace pid {
         }
 
         template <typename SizeType = std::uint32_t>
-        builder_offset<string<SizeType>> add_string(std::string_view s)
+        builder_offset<generic_string<SizeType>> add_string(std::string_view s)
         {
-            const auto size = (sizeof(SizeType) + s.size() + 1 + 3) / 4 * 4;
+            // TODO: alignment!
+            const auto total_size = (sizeof(SizeType) + s.size() + 1 + 3) / 4 * 4;
             const auto offset{data.size()};
-            data.resize(data.size() + size);
-            builder_offset<string<SizeType>> result{*this, offset};
-            result->size = s.size();
+            data.resize(data.size() + total_size);
+            builder_offset<generic_string<SizeType>> result{*this, offset};
+            result->string_length = s.size();
             std::memcpy(result->data, s.begin(), s.size());
             result->data[s.size()] = 0;
+
+            return result;
+        }
+
+        template <typename T, typename SizeType = std::uint32_t>
+        builder_offset<generic_vector<T, SizeType>> add_vector(SizeType size)
+        {
+            // TODO: alignment!
+            const auto total_size = sizeof(SizeType) * size;
+            const auto offset{data.size()};
+            data.resize(data.size() + total_size);
+
+            builder_offset<generic_vector<T, SizeType>> result{*this, offset};
+            result->vector_length = size;
 
             return result;
         }
