e628fe16aece7468b9213faa946e2ef457fda4fb
Frank Reininghaus <frank78ac@googlemail.com>
2023-09-11 10:21:54 +0200

    Prettify debug output

--- test.cpp
+++ test.cpp
@@ -12,8 +12,8 @@ void dump(const std::vector<char> & data)
     const auto ints{reinterpret_cast<const std::int32_t *>(data.data())};
 
     for (std::size_t index{0}; index < (data.size() + 3) / 4; ++index) {
-        std::cout << std::setw(4) << 4 * index << ".." << std::min(4 * index + 3, data.size() - 1)
-                  << ": ";
+        std::cout << std::setw(3) << std::setfill(' ') << 4 * index << ".." << std::setw(3)
+                  << std::setfill('.') << std::min(4 * index + 3, data.size() - 1) << ": ";
 
         for (std::size_t i{0}; i < 4; ++i) {
             const auto byteIndex{4 * index + 1};
@@ -32,12 +32,14 @@ void dump(const std::vector<char> & data)
 
         std::cout << "  (";
         for (std::size_t i{0}; i < 4 && 4 * index + i < data.size(); ++i) {
-            std::cout << " " << std::int32_t{data[4 * index + i]};
+            std::cout << " " << std::setw(3) << std::setfill(' ')
+                      << std::int32_t{data[4 * index + i]};
         }
         std::cout << " )";
 
         if (4 * index + 4 <= data.size()) {
-            std::cout << " (" << ints[index] << ")";
+            std::cout << " (" << std::setw(3) << ints[index] << " -> [" << 4 * index + ints[index]
+                      << "])";
         }
 
         std::cout << std::endl;
