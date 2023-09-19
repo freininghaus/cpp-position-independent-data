f9f545f27c2fe9d7227cbef6bdde64119d69ef93
Frank Reininghaus <frank78ac@googlemail.com>
2023-09-19 21:46:59 +0200

    Wrap data structure building in a class

--- pid-build-datastructures.h
+++ pid-build-datastructures.h
@@ -58,59 +58,65 @@ namespace pid {
     {
     };
 
-    template <
-        typename T,
-        typename = std::enable_if<std::is_arithmetic<T>::value || std::is_enum<T>::value, bool>>
-    T build(pid::builder &, T value)
+    struct datastructure_builder
     {
-        return value;
-    }
+        pid::builder & b;
 
-    inline builder_offset<string32> build(pid::builder & b, const std::string & s)
-    {
-        return b.add_string(s);
-    }
+        template <
+            typename T, typename = std::enable_if<
+                            std::is_arithmetic<T>::value || std::is_enum<T>::value, bool>>
+        T operator()(T value)
+        {
+            return value;
+        }
 
-    template <typename T>
-    auto build(pid::builder & b, const std::optional<T> & o)
-    {
-        if constexpr (std::is_arithmetic<T>::value || std::is_enum<T>::value) {
-            return o;
-        } else {
-            if (o) {
-                return build(b, *o);
+        inline builder_offset<string32> operator()(const std::string & s)
+        {
+            return b.add_string(s);
+        }
+
+        template <typename T>
+        auto operator()(const std::optional<T> & o)
+        {
+            if constexpr (std::is_arithmetic<T>::value || std::is_enum<T>::value) {
+                return o;
             } else {
-                return builder_offset<typename pid_base_type<T>::type>{b};
+                if (o) {
+                    return (*this)(*o);
+                } else {
+                    return builder_offset<typename pid_base_type<T>::type>{b};
+                }
             }
         }
-    }
 
-    template <typename T>
-    inline builder_offset<vector32<typename pid_type<T>::type>> build(
-        pid::builder & b, const std::vector<T> & v)
-    {
-        builder_offset<vector32<typename pid_type<T>::type>> result =
-            b.add_vector<typename pid_type<T>::type, std::uint32_t>(v.size());
+        template <typename T>
+        inline builder_offset<vector32<typename pid_type<T>::type>> operator()(
+            const std::vector<T> & v)
+        {
+            builder_offset<vector32<typename pid_type<T>::type>> result =
+                b.add_vector<typename pid_type<T>::type, std::uint32_t>(v.size());
 
-        for (std::size_t index{0}; index < v.size(); ++index) {
-            (*result)[index] = build(b, v[index]);
+            for (std::size_t index{0}; index < v.size(); ++index) {
+                (*result)[index] = (*this)(v[index]);
+            }
+
+            return result;
         }
 
-        return result;
-    }
-
-    template <typename Key, typename Value>
-    builder_offset<map32<typename pid_type<Key>::type, typename pid_type<Value>::type>> build(
-        pid::builder & b, const std::map<Key, Value> & m)
-    {
-        auto result{
-            b.add_map<typename pid_type<Key>::type, typename pid_type<Value>::type, std::uint32_t>(
+        template <typename Key, typename Value>
+        builder_offset<map32<typename pid_type<Key>::type, typename pid_type<Value>::type>>
+        operator()(const std::map<Key, Value> & m)
+        {
+            auto result{b.add_map<
+                typename pid_type<Key>::type, typename pid_type<Value>::type, std::uint32_t>(
                 m.size())};
 
-        for (const auto & [key, value] : m) {
-            *result.add_key(build(b, key)) = build(b, value);
-        }
+            for (const auto & [key, value] : m) {
+                *result.add_key((*this)(key)) = (*this)(value);
+            }
 
-        return result.offset();
-    }
+            return result.offset();
+        }
+    };
+
 }
